1a5b1057e5a5b454cdbe89250a786b1ccece59b3
LBecKX <lb@1601.com>
2019-01-11 07:44:47 +0100

    Change package name for pack a gist

--- src/Resources/contao/dca/tl_files.php
+++ src/Resources/contao/dca/tl_files.php
@@ -1,4 +1,5 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_files']['fields']['meta']['eval']['metaFields']['title'] .= " required";
+
 $GLOBALS['TL_DCA']['tl_files']['fields']['meta']['eval']['metaFields']['alt'] .= " required";
